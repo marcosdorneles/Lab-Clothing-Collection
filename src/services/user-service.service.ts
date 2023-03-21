aedf201626f4536cfcd191990d29281ee2f7013b
Marcos Guilherme Dorneles Staub <marcosguilhermedornelesstaub@MacBook-Air-de-Marcos.local>
2023-03-21 14:21:33 -0300

    preparando albiente de desenvolvimento

--- src/services/user-service.service.ts
+++ src/services/user-service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { User } from 'src/common/user';
-import { API_PATH } from 'src/environments/environments';
+import { User } from 'src/common/iuser';
+import { API_PATH } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +11,6 @@ export class UserServiceService {
   constructor(private httpClient: HttpClient) { }
 
   getUsers():Observable<User[]>{
-    return this.httpClient.get<User[]>(`${API_
+    return this.httpClient.get<User[]>(`${API_PATH}`)
+}
 }
